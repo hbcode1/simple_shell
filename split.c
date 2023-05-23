1351003982f50f939dee979a1c1e9205b8c3ca63
hbcode1 <houcine.bh@pm.me>
2023-05-23 21:13:29 +0100

    add split args function

---
+++ split.c
@@ -0,0 +1,35 @@
+#include "sshell.h"
+
+char **split_args_by_delim(char *str, char *delim, int *n)
+{
+	char **words, *tok;
+	int i = 0, j = 0, count = 0;
+
+	if (!str)
+		return (NULL);
+	/* count limits */
+	while (*(str + i) != '\0')
+	{
+		while (*(delim + j) != '\0')
+		{
+			if (*(str + i) == *(delim + j))
+				count++;
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	tok = strtok(str, delim);
+	words = malloc((count + 2) * sizeof(char *));
+	i = 0;
+	while (tok)
+	{
+		words[i] = malloc(_strlen(tok) + 1);
+		_strcpy(words[i], tok);
+		i++;
+		tok = strtok(NULL, delim);
+	}
+	words[i] = NULL;
+	*n = count + 1;
+	return (words);
+}
