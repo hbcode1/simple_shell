15b690ac7cca8ab63720903336c9c5785ef4ef58
hbcode1 <houcine.bh@pm.me>
2023-05-24 01:28:30 +0100

    print to stdout

--- prints.c
+++ prints.c
@@ -59,12 +59,42 @@ void handle_ctrl_c(int sig __attribute__((unused)))
  * @str: string to print
  */
 
-void putserr(char *str)
+void _putserr(char *str)
 {
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		_puterr(str[i]);
+	}
+}
+
+/**
+ * _putc - print a character to stdout
+ * @c: The character to print
+ *
+ * Return:
+ * - 1 Success
+ * - 1 Error and errno is set appropriately.
+ */
+
+int _putc(char c)
+{
+	return (write(STDOUT_FILENO, &c, 1));
+}
+
+/**
+ * _puts - print a string to stdout
+ *
+ * @str: string to print
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putc(str[i]);
 	}
 }
